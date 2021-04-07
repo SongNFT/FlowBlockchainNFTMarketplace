85014bbc63eb40851393f2b07bee1b0ce30e5a30
Eli Finkel <eligfinkel@gmail.com>
2021-04-06 21:29:01 -0500

    Added market place and pinnie crypto currency

--- Em-pinata-party/cadence/contracts/pinnieToken.cdc
+++ Em-pinata-party/cadence/contracts/pinnieToken.cdc
@@ -1,4 +1,4 @@
-pub contract pinnieToken{
+pub contract PinnieToken {
     pub var totalSupply: UFix64
     pub var tokenName: String
 
@@ -9,31 +9,54 @@ pub contract pinnieToken{
                     "Withdrawal amount must be the same as the balance of the withdrawn Vault"
             }
         }
-        pub resource interface Receiver {
-            pub fun deposit(from: @Vault)
-        }
-
-        pub resource interface Balance {
-            pub var balance: UFix64
-        }
-
-        pub resource Vault: Provider, Receiver, Balance {
-            pub var balance: UFix64
-
-            init(balance: UFix64) {
-                self.balance = balance
-            }
-
-            pub fun withdraw(amount: UFix64): @Vault {
-                self.balance = self.balance - amount
-                return <-create Vault(balance: amount)
-            }
-
-            pub fun deposit(from: @Vault) {
-                self.balance = self.balance + from.balance
-                destroy from
-            }
-        }
-        
     }
+    pub resource interface Receiver {
+        pub fun deposit(from: @Vault)
+    }
+
+    pub resource interface Balance {
+        pub var balance: UFix64
+    }
+
+    pub resource Vault: Provider, Receiver, Balance {
+        pub var balance: UFix64
+
+        init(balance: UFix64) {
+            self.balance = balance
+        }
+
+        pub fun withdraw(amount: UFix64): @Vault {
+            self.balance = self.balance - amount
+            return <-create Vault(balance: amount)
+        }
+
+        pub fun deposit(from: @Vault) {
+            self.balance = self.balance + from.balance
+            destroy from
+        }
+    }
+    pub fun createEmptyVault(): @Vault {
+     return <-create Vault(balance: 0.0)
+    }
+        
+    pub resource VaultMinter {
+        pub fun mintTokens(amount: UFix64, recipient: Capability<&AnyResource{Receiver}>) {
+            let recipientRef = recipient.borrow()
+                ?? panic("Could not borrow a receiver reference to the vault")
+
+            PinnieToken.totalSupply = PinnieToken.totalSupply + UFix64(amount)
+            recipientRef.deposit(from: <-create Vault(balance: amount))
+        }
+    }   
+    init() {
+        self.totalSupply = 30.0
+        self.tokenName = "Pinnie"
+
+        let vault <- create Vault(balance: self.totalSupply)
+        self.account.save(<-vault, to: /storage/MainVault)
+
+        self.account.save(<-create VaultMinter(), to: /storage/MainMinter)
+
+        self.account.link<&VaultMinter>(/private/Minter, target: /storage/MainMinter)
+    }   
 }
--- Em-pinata-party/pinata-party-frontend/src/TokenData.js
+++ Em-pinata-party/pinata-party-frontend/src/TokenData.js
@@ -1,55 +1,101 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import * as fcl from "@onflow/fcl";
+import * as t from "@onflow/types"
 
 const TokenData = () => {
-  const [nftInfo, setNftInfo] = useState(null)
-  const fetchTokenData = async () => {
-    const encoded = await fcl
-      .send([
+  
+  const [tokensToSell, setTokensToSell] = useState([])
+  
+  useEffect(() => {    
+    checkMarketplace()
+  }, []);
+
+  const checkMarketplace = async () => {
+    try {
+      const encoded = await fcl.send([
         fcl.script`
-        import PinataPartyContract from 0xf8d6e0586b0a20c7
-        pub fun main() : {String : String} {
-          let nftOwner = getAccount(0xf8d6e0586b0a20c7)  
-          let capability = nftOwner.getCapability<&{PinataPartyContract.NFTReceiver}>(/public/NFTReceiver)
-      
-          let receiverRef = capability.borrow()
-              ?? panic("Could not borrow the receiver reference")
-      
-          return receiverRef.getMetadata(id: 1)
+       import MarketplaceContract from 0xf8d6e0586b0a20c7
+        pub fun main(): [UInt64] {
+            let account1 = getAccount(0xf8d6e0586b0a20c7)
+            let acct1saleRef = account1.getCapability<&AnyResource{MarketplaceContract.SalePublic}>(/public/NFTSale)
+                .borrow()
+                ?? panic("Could not borrow acct2 nft sale reference")
+            return acct1saleRef.getIDs()
         }
-      `
-      ])
+        `
+      ]);
+      const decoded = await fcl.decode(encoded);
+      let marketplaceMetadata = [];
+      for (const id of decoded) {
+        const encodedMetadata = await fcl.send([
+          fcl.script`
+            import PinataPartyContract from 0xf8d6e0586b0a20c7
+            pub fun main(id: Int) : {String : String} {
+              let nftOwner = getAccount(0xf8d6e0586b0a20c7)  
+              let capability = nftOwner.getCapability<&{PinataPartyContract.NFTReceiver}>(/public/NFTReceiver)
+              let receiverRef = capability.borrow()
+                  ?? panic("Could not borrow the receiver reference")
+              return receiverRef.getMetadata(id: 1)
+            }
+          `,
+          fcl.args([
+            fcl.arg(id, t.Int)    
+          ]),
+        ]);
     
-    const decoded = await fcl.decode(encoded)
-    setNftInfo(decoded)
-  };
+        const decodedMetadata = await fcl.decode(encodedMetadata);
+        const encodedPrice = await fcl.send([
+          fcl.script`
+            import MarketplaceContract from 0xf8d6e0586b0a20c7
+            pub fun main(id: UInt64): UFix64? {
+                let account1 = getAccount(0xf8d6e0586b0a20c7)
+                let acct1saleRef = account1.getCapability<&AnyResource{MarketplaceContract.SalePublic}>(/public/NFTSale)
+                    .borrow()
+                    ?? panic("Could not borrow acct nft sale reference")
+                return acct1saleRef.idPrice(tokenID: id)
+            }
+          `, 
+          fcl.args([
+            fcl.arg(id, t.UInt64)
+          ])
+        ])
+        const decodedPrice = await fcl.decode(encodedPrice)
+        decodedMetadata["price"] = decodedPrice;
+        marketplaceMetadata.push(decodedMetadata);
+        
+      }
+      console.log(marketplaceMetadata);
+      setTokensToSell(marketplaceMetadata)
+      
+    } catch (error) {
+      console.log("NO NFTs FOR SALE")
+    }    
+  }
   return (
     <div className="token-data">
-    <div className="center">
-      <button className="btn-primary" onClick={fetchTokenData}>Fetch Token Data</button>        
+      {
+        tokensToSell.map(token => {
+          return (
+            <div key={token.uri} className="listing">
+              <div>
+                <h3>{token.name}</h3>
+                <h4>Stats</h4>
+                <p>Artist: {token.artisit}</p>
+                <p>Age: {token.age}</p>
+                <p>Rating: {token.rating}</p>
+                <h4>Video</h4>
+                <video loop={true} preload="auto" width="85%">
+                  <source src={`https://ipfs.io/ipfs/${token.uri.split("://")[1]}`} type="video/mp4" />
+                </video>
+                <h4>Price</h4>
+                <p>{parseInt(token.price, 10).toFixed(2)} Pinnies</p>
+                <button className="btn-primary">Buy Now</button>
+              </div>
+            </div>
+          )
+        })
+      }
     </div>
-    {
-      nftInfo &&
-      <div>
-        {
-          Object.keys(nftInfo).map(k => {
-            return (
-              <p>{k}: {nftInfo[k]}</p>
-            )
-          })
-        }
-        <div className="center video">
-          <video id="nft-video" canplaythrough controls width="85%">
-            <source src={`https://ipfs.io/ipfs/${nftInfo["uri"].split("://")[1]}`}
-                  type="video/mp4" />
-          </video>
-          <div>
-            <button onClick={() => setNftInfo(null)} className="btn-secondary">Clear Token Info</button>
-          </div>
-        </div>          
-      </div>
-    }
-  </div>
   );
 };
 
